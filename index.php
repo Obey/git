9a3feb3bcbdda935265dbda0ae454641ae68bb2a
obeymyz <babaytg@gmail.com>
2021-08-25 18:27:02 +0300

    AGA

--- index.php
+++ index.php
@@ -1,1 +1,6 @@
-!!!!!!!TTYYY
+
+<script type="module">
+import {Runtime, Inspector} from "https://cdn.jsdelivr.net/npm/@observablehq/runtime@4/dist/runtime.js";
+import define from "https://api.observablehq.com/@sethpipho/fractal-tree.js?v=3";
+new Runtime().module(define, Inspector.into("#observablehq-055a85a4"));
+</script>
